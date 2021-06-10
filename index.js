f6bd21f97b72b17ead985d6d30cc6cfb61171ba5
Shalveena <shalveena.s.prasad@gmail.com>
2021-06-10 11:52:52 +1000

    add first validation logic

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-let validFormFieldInput = function (data) {};
+function validFormFieldInput(data) {
+  const newNameInput = document.querySelector("#new-name");
+  const taskName = document.querySelector("#task-name");
+  const newAssignInput = document.querySelector("#new-assign");
+  const newDescriptInput = document.querySelector("#new-descript");
+  const newStatusInput = document.querySelector("#new-status");
+  const newDueDate = document.querySelector("#new-duedate");
+  const taskNameErr = document.querySelector("#tn-error-msg");
+
+  // console.log("name: " + newNameInput.value);
+
+  // check if Task Name is more than 5 characters
+
+  if (newNameInput.value == "" || newNameInput.value.length < 5) {
+    taskNameErr.innerHTML = "Length should be more than 5";
+    taskNameErr.style.color = "#ff0000";
+    newNameInput.focus();
+  } else {
+    taskNameErr.innerHTML = "All good ";
+    taskNameErr.style.color = "green";
+  }
+}
+
+addBtn.addEventListener("click", validFormFieldInput);
