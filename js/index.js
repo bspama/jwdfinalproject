fd95e41ae132423e87ed36616c6434fd046d5827
Boots Pama <boots.pama@gmail.com>
2021-06-10 17:00:20 +1000

    Written Pseudo Codes for Task 6

--- js/index.js
+++ js/index.js
@@ -78,4 +78,15 @@ function validFormFieldInput(data) {
   event.preventDefault();
 }
 
+//1. When we submit, we need check all the inputs are valid first
 addBtn.addEventListener("click", validFormFieldInput);
+
+//2. we need to call the addTask method from the Task Manager Task and 
+
+//3. Pass the form's input as parameter
+
+//4. Clear values for next submission
+
+
+
+
